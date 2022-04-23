e0f4bb464c48e8cdcbe796abf9db3dc4f80bf9cd
Nanashi Li <tihan-nico@plexus.dev>
2022-04-23 02:53:40 +0200

    Revert "Set path correctly"
    This reverts commit 567948c6aff2528dd16f2cb269833133cea9a40a.

--- api/github/webhooks/index.js
+++ api/github/webhooks/index.js
@@ -1,6 +1,6 @@
 const { createNodeMiddleware, createProbot } = require("probot");
 
-const app = require("../../app");
+const app = require('../../../app');
 
 module.exports = createNodeMiddleware(app, {
   probot: createProbot(),
